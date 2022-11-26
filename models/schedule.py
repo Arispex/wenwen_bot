f2f7637be082643c99598da5bbd4df07528e7d8d
Qianyiovo <qianyiovo@outlook.com>
2022-11-26 16:53:58 +0800

    新增 获取全部 和 获取有效日程

--- models/schedule.py
+++ models/schedule.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 import models.orm
+import datetime
 
 
 class Schedule(BaseModel):
@@ -18,3 +19,17 @@ class Schedule(BaseModel):
     @classmethod
     async def create(cls, name: str, time: str, content: str):
         return cls.from_orm(await models.orm.Schedule.create(name=name, time=time, content=content))
+
+    @classmethod
+    async def get_all(cls):
+        return [cls.from_orm(schedule) for schedule in await models.orm.Schedule.all()]
+
+    @classmethod
+    async def get_valid(cls):
+        schedules = await models.orm.Schedule.all()
+        for i in schedules:
+            if datetime.datetime.strptime(i.time, "%Y-%m-%d %H:%M") < datetime.datetime.now():
+                schedules.remove(i)
+
+        schedules.sort(key=lambda x: datetime.datetime.strptime(x.time, "%Y-%m-%d %H:%M"))
+        return schedules
