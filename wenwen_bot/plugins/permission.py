e8ea21109a9a0880fc478c3f9cb7cc23816d2119
Qianyiovo <qianyiovo@outlook.com>
2022-11-26 17:12:07 +0800

    新增 群号判断

--- wenwen_bot/plugins/permission.py
+++ wenwen_bot/plugins/permission.py
@@ -1,16 +1,15 @@
-from nonebot import on_command, on_message
+from nonebot import on_command
 from nonebot.log import logger
 from nonebot.params import CommandArg
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import (
     Message,
-    MessageEvent,
     GroupMessageEvent,
-    PrivateMessageEvent, Bot,
+    Bot,
 )
 from nonebot_plugin_tortoise_orm import add_model
-import models
 import json
+import config
 
 add_model("models.orm")
 
@@ -21,45 +20,47 @@ remove_admin = on_command("删除管理员", permission=SUPERUSER)
 
 @add_admin.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
-    user_id = args.extract_plain_text()
-    if user_id:
-        try:
-            with open("admins.json", "r") as fp:
-                admins = json.load(fp)
-        except FileNotFoundError:
-            admins = []
+    if event.group_id in config.groups:
+        user_id = args.extract_plain_text()
+        if user_id:
+            try:
+                with open("admins.json", "r") as fp:
+                    admins = json.load(fp)
+            except FileNotFoundError:
+                admins = []
 
-        admins.append(user_id)
-        with open("admins.json", "w") as fp:
-            json.dump(admins, fp)
+            admins.append(user_id)
+            with open("admins.json", "w") as fp:
+                json.dump(admins, fp)
 
-        logger.info(f"「{event.user_id} 」添加了管理员「{user_id}」")
-        logger.info("管理员列表已更新")
-        await add_admin.finish(f"添加成功！")
-    else:
-        await add_admin.finish(f"无效的语法！\n正确的语法：/添加管理员 <QQ号>")
+            logger.info(f"「{event.user_id} 」添加了管理员「{user_id}」")
+            logger.info("管理员列表已更新")
+            await add_admin.finish(f"添加成功！")
+        else:
+            await add_admin.finish(f"无效的语法！\n正确的语法：/添加管理员 <QQ号>")
 
 
 @remove_admin.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
-    user_id = args.extract_plain_text()
-    if user_id:
-        try:
-            with open("admins.json", "r") as fp:
-                admins = json.load(fp)
-        except FileNotFoundError:
-            admins = []
-            with open("admins.json", "w") as fp:
-                json.dump(admins, fp)
-        if user_id in admins:
-            admins.remove(user_id)
-            with open("admins.json", "w") as fp:
-                json.dump(admins, fp)
+    if event.group_id in config.groups:
+        user_id = args.extract_plain_text()
+        if user_id:
+            try:
+                with open("admins.json", "r") as fp:
+                    admins = json.load(fp)
+            except FileNotFoundError:
+                admins = []
+                with open("admins.json", "w") as fp:
+                    json.dump(admins, fp)
+            if user_id in admins:
+                admins.remove(user_id)
+                with open("admins.json", "w") as fp:
+                    json.dump(admins, fp)
 
-            logger.info(f"「{event.user_id} 」删除了管理员「{user_id}」")
-            logger.info("管理员列表已更新")
-            await remove_admin.finish(f"删除成功！")
+                logger.info(f"「{event.user_id} 」删除了管理员「{user_id}」")
+                logger.info("管理员列表已更新")
+                await remove_admin.finish(f"删除成功！")
+            else:
+                await remove_admin.finish(f"该用户不是管理员！")
         else:
-            await remove_admin.finish(f"该用户不是管理员！")
-    else:
-        await remove_admin.finish(f"无效的语法！\n正确的语法：/删除管理员 <QQ号>")
+            await remove_admin.finish(f"无效的语法！\n正确的语法：/删除管理员 <QQ号>")
--- wenwen_bot/plugins/schedule.py
+++ wenwen_bot/plugins/schedule.py
@@ -1,14 +1,13 @@
-from nonebot import on_command, on_message
+from nonebot import on_command
 from nonebot.log import logger
 from nonebot.params import CommandArg
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import (
     Message,
-    MessageEvent,
     GroupMessageEvent,
-    PrivateMessageEvent, Bot,
+    Bot,
 )
-from nonebot_plugin_tortoise_orm import add_model
+import config
 import models
 import datetime
 
@@ -19,61 +18,65 @@ remove_schedule = on_command("删除日程", permission=SUPERUSER | models.permi
 
 @add_schedule.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
-    params = args.extract_plain_text().split(" ")
-    # three params: title, time, content
-    if len(params) == 4:
-        title = params[0]
-        time = " ".join(params[2:])
-        content = params[1]
+    if event.group_id in config.groups:
+        params = args.extract_plain_text().split(" ")
+        # three params: title, time, content
+        if len(params) == 4:
+            title = params[0]
+            time = " ".join(params[2:])
+            content = params[1]
 
-        # verify time format(yyyy-mm-dd hh:mm)
-        try:
-            time_obj = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M")
-        except ValueError:
+            # verify time format(yyyy-mm-dd hh:mm)
             try:
-                time_obj = datetime.datetime.strptime(time, "%m-%d %H:%M")
+                time_obj = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M")
             except ValueError:
-                await add_schedule.finish("时间格式错误！正确格式：yyyy-mm-dd hh:mm 或 mm-dd hh:mm")
+                try:
+                    time_obj = datetime.datetime.strptime(time, "%m-%d %H:%M")
+                except ValueError:
+                    await add_schedule.finish("时间格式错误！正确格式：yyyy-mm-dd hh:mm 或 mm-dd hh:mm")
 
-        # Correction time
-        if time_obj.year == 1900:
-            time_obj = time_obj.replace(year=datetime.datetime.now().year)
-            time = time_obj.strftime("%Y-%m-%d %H:%M")
+            # Correction time
+            if time_obj.year == 1900:
+                time_obj = time_obj.replace(year=datetime.datetime.now().year)
+                time = time_obj.strftime("%Y-%m-%d %H:%M")
 
-        # verify time is in the future
-        if time_obj < datetime.datetime.now():
-            await add_schedule.finish("时间不能在过去！")
+            # verify time is in the future
+            if time_obj < datetime.datetime.now():
+                await add_schedule.finish("时间不能在过去！")
 
-        # verify title is not empty
-        if not title:
-            await add_schedule.finish("标题不能为空！")
+            # verify title is not empty
+            if not title:
+                await add_schedule.finish("标题不能为空！")
 
-        # verify content is not empty
-        if not content:
-            await add_schedule.finish("内容不能为空！")
+            # verify content is not empty
+            if not content:
+                await add_schedule.finish("内容不能为空！")
 
-        # add schedule
-        await models.Schedule.create(
-            name=title,
-            time=time,
-            content=content,
-        )
+            # add schedule
+            await models.Schedule.create(
+                name=title,
+                time=time,
+                content=content,
+            )
 
-        await add_schedule.finish("添加成功！")
+            logger.info(f"「{event.get_user_id()}」添加了一个新日程「{title}」")
+            await add_schedule.finish("添加成功！")
 
-    else:
-        await add_schedule.finish(f"无效的语法！\n正确的语法：/添加日程 <标题> <时间> <内容>")
+        else:
+            await add_schedule.finish(f"无效的语法！\n正确的语法：/添加日程 <标题> <时间> <内容>")
 
 
 @remove_schedule.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
-    schedule_id = int(args.extract_plain_text())
-    if schedule_id:
-        schedules = await models.Schedule.get_valid()
-        if schedule_id > len(schedules):
-            await remove_schedule.finish("日程不存在！")
+    if event.group_id in config.groups:
+        schedule_id = int(args.extract_plain_text())
+        if schedule_id:
+            schedules = await models.Schedule.get_valid()
+            if schedule_id > len(schedules):
+                await remove_schedule.finish("日程不存在！")
+            else:
+                logger.info(f"「{event.get_user_id()}」删除了一个日程「{schedules[schedule_id - 1].name}」")
+                await schedules[schedule_id - 1].delete()
+                await remove_schedule.finish("删除成功！")
         else:
-            await schedules[schedule_id - 1].delete()
-            await remove_schedule.finish("删除成功！")
-    else:
-        await remove_schedule.finish("无效的语法！\n正确的语法：/删除日程 <日程ID>")
+            await remove_schedule.finish("无效的语法！\n正确的语法：/删除日程 <日程ID>")
